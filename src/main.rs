bc83795c023a072e0c38e3ab95754c342f2c624d
zehreken <zehreken@gmail.com>
2021-03-07 00:01:34 +0100

    Add eframe to test egui for modul

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod beat_controller;
+mod egui_view;
 mod modul;
 mod modul_utils;
 mod nannou_view;
 mod tape;
 
 fn main() {
-    nannou_view::start();
+    // nannou_view::start();
+    egui_view::start();
 }
