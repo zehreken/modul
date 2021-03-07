aaa43e248c396ad3e549bbf4d1492c00121b54ce
zehreken <zehreken@gmail.com>
2021-03-07 23:31:20 +0100

    Improve egui_view

--- src/egui_view.rs
+++ src/egui_view.rs
@@ -1,24 +1,22 @@
 use super::modul;
 use eframe::{
-    egui::{self, color, Key},
+    egui::{self, Key, *},
     epi,
 };
-use egui::Ui;
+use std::time::Instant;
 
 struct EguiView {
+    instant: Instant,
     modul: modul::Modul,
     selected_tape: usize,
-    name: String,
-    age: u32,
 }
 
 impl Default for EguiView {
     fn default() -> Self {
         Self {
+            instant: Instant::now(),
             modul: modul::Modul::new(),
             selected_tape: 0,
-            name: "Arthur".to_owned(),
-            age: 42,
         }
     }
 }
@@ -30,40 +28,80 @@ impl epi::App for EguiView {
 
     fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
         let Self {
+            instant,
             modul,
             selected_tape,
-            name,
-            age,
         } = self;
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
+            ctx.request_repaint();
+            ui.heading("modul");
+
+            // ui.add(egui::Slider::u32(age, 0..=120).text("age"));
+            ui.label(format!(
+                "real time: {:0.1}",
+                instant.elapsed().as_secs_f32()
+            ));
+            ui.label(format!("modul time: {:0.1}", modul.get_audio_index()));
+            ui.label(format!("diff: {:0.5}", 0.0));
             ui.horizontal(|ui| {
-                ui.label("Your name: ");
-                ui.text_edit_singleline(name);
-            });
-            ui.add(egui::Slider::u32(age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                *age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", name, age));
-            ui.horizontal(|ui| {
-                if ui.selectable_label(*selected_tape == 0, "1").clicked() {
-                    *selected_tape = 0;
-                    modul.set_selected_tape(0);
-                }
-                if ui.selectable_label(*selected_tape == 1, "2").clicked() {
-                    *selected_tape = 1;
-                    modul.set_selected_tape(1);
-                }
-                if ui.selectable_label(*selected_tape == 2, "3").clicked() {
-                    *selected_tape = 2;
-                    modul.set_selected_tape(2);
-                }
-                if ui.selectable_label(*selected_tape == 3, "4").clicked() {
-                    *selected_tape = 3;
-                    modul.set_selected_tape(3);
-                }
+                ui.group(|ui| {
+                    if ui.selectable_label(*selected_tape == 0, "1").clicked() {
+                        *selected_tape = 0;
+                        modul.set_selected_tape(0);
+                    }
+                    let time = ui.input().time;
+
+                    let desired_size = ui.available_width() * vec2(0.5, 0.2);
+                    let (_id, rect) = ui.allocate_space(desired_size);
+
+                    let to_screen = emath::RectTransform::from_to(
+                        Rect::from_x_y_ranges(0.0..=1.0, -1.0..=1.0),
+                        rect,
+                    );
+
+                    let mut shapes = vec![];
+
+                    for &mode in &[2, 3, 5] {
+                        let mode = mode as f32;
+                        let n = 60;
+                        let speed = 1.5;
+
+                        let points: Vec<Pos2> = (0..=n)
+                            .map(|i| {
+                                let t = i as f32 / (n as f32);
+                                let amp = (time as f32 * speed * mode).sin() / mode;
+                                let y = amp * (t * std::f32::consts::TAU / 2.0 * mode).sin();
+                                to_screen * pos2(t, y)
+                            })
+                            .collect();
+
+                        let thickness = 10.0 / mode;
+                        shapes.push(epaint::Shape::line(
+                            points,
+                            Stroke::new(thickness, Color32::from_additive_luminance(196)),
+                        ));
+                    }
+                    ui.painter().extend(shapes);
+                });
+                ui.group(|ui| {
+                    if ui.selectable_label(*selected_tape == 1, "2").clicked() {
+                        *selected_tape = 1;
+                        modul.set_selected_tape(1);
+                    }
+                });
+                ui.group(|ui| {
+                    if ui.selectable_label(*selected_tape == 2, "3").clicked() {
+                        *selected_tape = 2;
+                        modul.set_selected_tape(2);
+                    }
+                });
+                ui.group(|ui| {
+                    if ui.selectable_label(*selected_tape == 3, "4").clicked() {
+                        *selected_tape = 3;
+                        modul.set_selected_tape(3);
+                    }
+                });
             });
 
             // Keyboard input
@@ -77,19 +115,26 @@ impl epi::App for EguiView {
                         if !pressed {
                             match key {
                                 Key::Num1 => {
+                                    *selected_tape = 0;
                                     modul.set_selected_tape(0);
                                 }
                                 Key::Num2 => {
+                                    *selected_tape = 1;
                                     modul.set_selected_tape(1);
                                 }
                                 Key::Num3 => {
+                                    *selected_tape = 2;
                                     modul.set_selected_tape(2);
                                 }
                                 Key::Num4 => {
+                                    *selected_tape = 3;
                                     modul.set_selected_tape(3);
                                 }
                                 Key::R => {
                                     modul.record();
+                                }
+                                Key::Escape => {
+                                    frame.quit();
                                 }
                                 _ => {}
                             }
@@ -101,7 +146,8 @@ impl epi::App for EguiView {
         });
 
         // Resize the native window to be just the size we need it to be:
-        frame.set_window_size(ctx.used_size());
+        // frame.set_window_size(ctx.used_size());
+        frame.set_window_size(vec2(512.0, 256.0));
     }
 }
 
