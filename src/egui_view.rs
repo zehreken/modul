d4f4eaad22c9a983799a0e8dc84062baebcfbda1
zehreken <zehreken@gmail.com>
2021-03-08 23:00:48 +0100

    Improve UI

--- src/egui_view.rs
+++ src/egui_view.rs
@@ -44,65 +44,9 @@ impl epi::App for EguiView {
             ));
             ui.label(format!("modul time: {:0.1}", modul.get_audio_index()));
             ui.label(format!("diff: {:0.5}", 0.0));
-            ui.horizontal(|ui| {
-                ui.group(|ui| {
-                    if ui.selectable_label(*selected_tape == 0, "1").clicked() {
-                        *selected_tape = 0;
-                        modul.set_selected_tape(0);
-                    }
-                    let time = ui.input().time;
-
-                    let desired_size = ui.available_width() * vec2(0.5, 0.2);
-                    let (_id, rect) = ui.allocate_space(desired_size);
-
-                    let to_screen = emath::RectTransform::from_to(
-                        Rect::from_x_y_ranges(0.0..=1.0, -1.0..=1.0),
-                        rect,
-                    );
-
-                    let mut shapes = vec![];
-
-                    for &mode in &[2, 3, 5] {
-                        let mode = mode as f32;
-                        let n = 60;
-                        let speed = 1.5;
-
-                        let points: Vec<Pos2> = (0..=n)
-                            .map(|i| {
-                                let t = i as f32 / (n as f32);
-                                let amp = (time as f32 * speed * mode).sin() / mode;
-                                let y = amp * (t * std::f32::consts::TAU / 2.0 * mode).sin();
-                                to_screen * pos2(t, y)
-                            })
-                            .collect();
-
-                        let thickness = 10.0 / mode;
-                        shapes.push(epaint::Shape::line(
-                            points,
-                            Stroke::new(thickness, Color32::from_additive_luminance(196)),
-                        ));
-                    }
-                    ui.painter().extend(shapes);
-                });
-                ui.group(|ui| {
-                    if ui.selectable_label(*selected_tape == 1, "2").clicked() {
-                        *selected_tape = 1;
-                        modul.set_selected_tape(1);
-                    }
-                });
-                ui.group(|ui| {
-                    if ui.selectable_label(*selected_tape == 2, "3").clicked() {
-                        *selected_tape = 2;
-                        modul.set_selected_tape(2);
-                    }
-                });
-                ui.group(|ui| {
-                    if ui.selectable_label(*selected_tape == 3, "4").clicked() {
-                        *selected_tape = 3;
-                        modul.set_selected_tape(3);
-                    }
-                });
-            });
+            for i in 0..4 {
+                group(ui, selected_tape, modul, i);
+            }
 
             // Keyboard input
             for e in ui.input().events.iter() {
@@ -112,7 +56,7 @@ impl epi::App for EguiView {
                         pressed,
                         modifiers,
                     } => {
-                        if !pressed {
+                        if *pressed {
                             match key {
                                 Key::Num1 => {
                                     *selected_tape = 0;
@@ -147,8 +91,51 @@ impl epi::App for EguiView {
 
         // Resize the native window to be just the size we need it to be:
         // frame.set_window_size(ctx.used_size());
-        frame.set_window_size(vec2(512.0, 256.0));
+        frame.set_window_size(vec2(512.0, 512.0));
     }
+}
+
+fn group(ui: &mut Ui, selected_tape: &mut usize, modul: &mut modul::Modul, id: usize) {
+    ui.group(|ui| {
+        if ui
+            .selectable_label(*selected_tape == id, (id + 1).to_string())
+            .clicked()
+        {
+            *selected_tape = id;
+            modul.set_selected_tape(id);
+        }
+        let time = ui.input().time;
+
+        let desired_size = ui.available_width() * vec2(1.0, 0.1);
+        let (_id, rect) = ui.allocate_space(desired_size);
+
+        let to_screen =
+            emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=1.0, -1.0..=1.0), rect);
+
+        let mut shapes = vec![];
+
+        for &mode in &[2, 3, 5] {
+            let mode = mode as f32;
+            let n = 60;
+            let speed = 1.5;
+
+            let points: Vec<Pos2> = (0..=n)
+                .map(|i| {
+                    let t = i as f32 / (n as f32);
+                    let amp = (time as f32 * speed * mode).sin() / mode;
+                    let y = amp * (t * std::f32::consts::TAU / 2.0 * mode).sin();
+                    to_screen * pos2(t, y)
+                })
+                .collect();
+
+            let thickness = 10.0 / mode;
+            shapes.push(epaint::Shape::line(
+                points,
+                Stroke::new(thickness, Color32::from_additive_luminance(196)),
+            ));
+        }
+        ui.painter().extend(shapes);
+    });
 }
 
 pub fn start() {
