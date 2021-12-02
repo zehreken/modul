55bff08fc62f9986483d6438757f527457415c83
zehreken <zehreken@gmail.com>
2021-12-02 20:19:55 +0100

    Improve metronome

--- src/view/windows/window_metronome.rs
+++ src/view/windows/window_metronome.rs
@@ -1,19 +1,22 @@
 use crate::modul;
 
-pub struct WindowMetronome {}
+pub struct WindowMetronome {
+    is_running: bool,
+}
 
 impl Default for WindowMetronome {
     fn default() -> Self {
-        Self {}
+        Self { is_running: false }
     }
 }
 
 impl WindowMetronome {
     pub fn draw(&mut self, ctx: &egui::CtxRef, modul: &mut modul::Modul) {
-        let Self {} = self;
+        let Self { is_running } = self;
         egui::Window::new("metronome").show(ctx, |ui| {
             ctx.request_repaint();
             ui.label(format!("time {}", 1234567890));
+            ui.checkbox(is_running, "run");
         });
     }
 }
