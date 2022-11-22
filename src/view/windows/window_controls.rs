8b0c75e93d0ad24392e02858bbab8c275350ca06
zehreken <zehreken@gmail.com>
2022-11-22 22:47:49 +0100

    Improve Controls window

--- src/view/windows/window_controls.rs
+++ src/view/windows/window_controls.rs
@@ -8,16 +8,19 @@ impl Drawable for WindowControls {
         egui::Window::new("controls").show(egui_ctx, |ui| {
             // ctx.request_repaint();
             ui.label("Controls");
-            ui.label("select tape [1-8]");
-            ui.label("select secondary tape shift+[1-8]");
-            ui.label("record tape toggle (space)");
+            ui.label("select tape ([1-8])");
+            ui.label("select secondary tape (shift+[1-8])");
+            ui.label("toggle record tape (Space)");
             ui.label("clear tape (C)");
-            ui.label("clear all tapes shift+C");
+            ui.label("clear all tapes (shift + C)");
             ui.label("mute/unmute tape (M)");
-            ui.label("record live toggle (T)");
-            ui.label("play-through toggle (Y)");
+            ui.label("solo/unsolo tape (S)");
+            ui.label("merge tapes (N)");
+            ui.label("toggle record live (T)");
+            ui.label("toggle play-through (Y)");
             ui.label("write to disc (W)");
             ui.label("tape volume (up/down)");
+            ui.label("quit (Esc)");
         });
     }
 }
